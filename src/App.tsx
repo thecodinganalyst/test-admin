e110f20579417eaf33e31c0d9bb8e695d574b02a
Dennis Cai <dennis.cai@singtel.com>
2024-09-25 16:53:13 +0800

    add dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,12 @@ import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
+import { Dashboard } from './Dashboard';
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}>
+  <Admin layout={Layout} dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} show={ShowGuesser} icon={UserIcon} />
   </Admin>
