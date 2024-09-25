0b73b925e3265fb2f4f92452d0a5e62e41f3636d
Dennis Cai <dennis.cai@singtel.com>
2024-09-25 16:51:15 +0800

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,12 @@ import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} show={ShowGuesser} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} show={ShowGuesser} icon={UserIcon} />
   </Admin>
 );
