20d37fc2b94eec4efc511249135f8cead8486987
Dennis Cai <dennis.cai@singtel.com>
2024-09-25 16:35:07 +0800

    add edit form for posts

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ import {
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
-import { PostList } from "./posts";
+import { PostList, PostEdit } from "./posts";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} edit={PostEdit} />
     <Resource name="users" list={UserList} show={ShowGuesser} />
   </Admin>
 );
--- src/posts.tsx
+++ src/posts.tsx
@@ -1,4 +1,4 @@
-import { Datagrid, List, ReferenceField, TextField, EditButton } from 'react-admin';
+import { Datagrid, List, ReferenceField, TextField, EditButton, Edit, SimpleForm, ReferenceInput, TextInput } from 'react-admin';
 
 export const PostList = () => (
     <List>
@@ -9,4 +9,15 @@ export const PostList = () => (
             <EditButton />
         </Datagrid>
     </List>
+);
+
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm>
+            <TextInput source="id" InputProps={{ disabled: true }} />
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="title" />
+            <TextInput source="body" multiline rows={5} />
+        </SimpleForm>
+    </Edit>
 );
