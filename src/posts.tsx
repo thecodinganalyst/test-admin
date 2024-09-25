d2393f3741c4e9036d3b9066cd74ce4cecc796d1
Dennis Cai <dennis.cai@singtel.com>
2024-09-25 16:25:09 +0800

    add edit button and remove description in posts

--- src/posts.tsx
+++ src/posts.tsx
@@ -1,12 +1,12 @@
-import { Datagrid, List, ReferenceField, TextField } from 'react-admin';
+import { Datagrid, List, ReferenceField, TextField, EditButton } from 'react-admin';
 
 export const PostList = () => (
     <List>
-        <Datagrid>
-            <ReferenceField source="userId" reference="users" />
+        <Datagrid rowClick={false}>
             <TextField source="id" />
+            <ReferenceField source="userId" reference="users" />
             <TextField source="title" />
-            <TextField source="body" />
+            <EditButton />
         </Datagrid>
     </List>
 );
