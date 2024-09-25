15678a8a782fb7d31eafdedf89a65e6ca38b5641
Dennis Cai <dennis.cai@singtel.com>
2024-09-25 16:58:22 +0800

    add authentication

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,12 @@ import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { Dashboard } from './Dashboard';
+import { authProvider } from './authProvider';
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin layout={Layout} dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} show={ShowGuesser} icon={UserIcon} />
   </Admin>
